bca9b4763e9131baf31a2b54357f06369024e0cb
unknown <dangabarin2020@gmail.com>
2025-10-21 12:13:32 +0100

    update commit

--- src/components/Section1.jsx
+++ src/components/Section1.jsx
@@ -1,3 +1,4 @@
+'use client'
 import { motion, useTransform, useScroll, useSpring, useInView} from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 import service01 from "../../public/images/service01.png"
@@ -15,6 +16,7 @@ import Paragraph from "@/typography/Paragraph";
 import GridColumn from "@/layout/GridColumn";
 import Heading2 from "@/typography/Heading2";
 import SlideUpElement from "@/effects/SlideUpElement";
+import { useMediaQuery } from "react-responsive";
 
 const Section1 = () => {
 
@@ -25,6 +27,7 @@ const Section1 = () => {
 };
 
 const HorizontalScrollCarousel = () => {
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' })
   const targetRef = useRef(null);
   const { scrollYProgress } = useScroll({
     target: targetRef,
@@ -36,7 +39,7 @@ const HorizontalScrollCarousel = () => {
       // ;console.log(1-scrollYProgress.current)
     })
   })
-  const x = useTransform(scrollYProgress, [0, 1], ["110%", "-70%"]);
+  const x = useTransform(scrollYProgress, [0, 1], [isTabletOrMobile?'150%':'90%', isTabletOrMobile?'-90%':'-70%']);
   const swiftx = useSpring(x, { stiffness: 120, damping: 30, mass: 0.3 });
 const texts=['h e l p i n g','l a n d','s c r a p e r s','s u c c e e d']
   return (
@@ -45,7 +48,6 @@ const texts=['h e l p i n g','l a n d','s c r a p e r s','s u c c e e d']
       <div className="sticky bgpurple-500 top-0 flex flex-col h-svh items-center justify-center overflow-hidden">
    {/* <div className="bgblue-600 grid grid-cols-12 gap5 h-full items-end place-content-end pb-20"> */}
    <GridColumn className={'bggreen-400'}>
-
     <motion.h2 style={{x:swiftx}} className='bgslate-400 col-start-0 col-span-full mask-gradien text-black break-words text-[3em]  lg:text-[8em] leading-[0.95] tracking-[-0.03em] lowercase text-center justify-center font-custom font-black flex flex-co flexwrap'>
           {texts.map((word,index) => {
             const total= texts.length
@@ -61,9 +63,11 @@ const texts=['h e l p i n g','l a n d','s c r a p e r s','s u c c e e d']
 
 
 
-          return <Text current={scrollYProgress} rangeForOnscroll={[start,end,end+0.3]} range={[start,end,end+0.1]} rang={[index* 0.125,(index* 0.125)*0.125+(index* 0.125+0.1)]} word={word} wowrd={(index* 0.125)+1-scrollYProgress.current} index={index}  key={index} />;
+          return <Text isTabletOrMobile={isTabletOrMobile} current={scrollYProgress} rangeForOnscroll={[start,end,end+0.3]} range={[start,end,end+0.1]} rang={[index* 0.125,(index* 0.125)*0.125+(index* 0.125+0.1)]} word={word} wowrd={(index* 0.125)+1-scrollYProgress.current} index={index}  key={index} />;
           })}
           </motion.h2> 
+ {/* {isTabletOrMobile && <p>You are a tablet or mobile phone</p>} */}
+
 
 <Paragraph className="text-black z-[9] px-5 pt-[80px] bg-slate600  lg:col-start-4 col-span-full lg:col-span-6 text-center">
 
@@ -79,14 +83,15 @@ const texts=['h e l p i n g','l a n d','s c r a p e r s','s u c c e e d']
   );
 };
 
-const Text = ({index,word,current,range,rangeForOnscroll}) => {
+const Text = ({isTabletOrMobile,index,word,current,range,rangeForOnscroll}) => {
+  
 
   const opacit = useTransform(current, rangeForOnscroll, [0, 0,1]);
    const opacity = useSpring(opacit, { stiffness: 120, damping: 30, mass: 0.3 });
 
   // const y = useTransform(current, range, [30, 0]);
   // const minusy = useTransform(current, range, [-28, 0]);
-  const origy = useTransform(current, range, [60, -18,0]);
+  const origy = useTransform(current, range, [5, -18,0]);
    const y = useSpring(origy, { stiffness: 120, damping: 30, mass: 0.3 });
   const minus = useTransform(current, range, [-60,18, 0]);
    const minusy = useSpring(minus, { stiffness: 120, damping: 30, mass: 0.3 });
